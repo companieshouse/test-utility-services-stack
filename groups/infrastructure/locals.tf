96218329a213fe19a1222bfe67b0656b30bdd494
bcullerton <bcullerton@companieshouse.gov.uk>
2025-10-08 10:30:56 +0100

    Add access from applications, admin and concourse to alb

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -3,15 +3,17 @@ locals {
   stack_fullname = "${local.stack_name}-stack"
   name_prefix    = "${local.stack_name}-${var.environment}"
 
-  stack_secrets              = jsondecode(data.vault_generic_secret.secrets.data_json)
+  stack_secrets = jsondecode(data.vault_generic_secret.secrets.data_json)
 
-  application_subnet_pattern = local.stack_secrets["application_subnet_pattern"]
-  application_subnet_ids     = join(",", data.aws_subnets.application.ids)
-  subnet_ids_private         = join(",", data.aws_subnets.private.ids)
-  kms_key_alias              = local.stack_secrets["kms_key_alias"]
-  vpc_name                   = local.stack_secrets["vpc_name"]
+  application_subnet_pattern  = local.stack_secrets["application_subnet_pattern"]
+  application_subnet_ids      = join(",", data.aws_subnets.application.ids)
+  kms_key_alias               = local.stack_secrets["kms_key_alias"]
+  vpc_name                    = local.stack_secrets["vpc_name"]
   notify_topic_slack_endpoint = local.stack_secrets["notify_topic_slack_endpoint"]
-  parameter_store_secrets    = {
-    "vpc-name"                 = local.stack_secrets["vpc_name"]
+  parameter_store_secrets = {
+    "vpc-name" = local.stack_secrets["vpc_name"]
   }
+
+  ingress_prefix_list_ids = [data.aws_ec2_managed_prefix_list.admin.id, data.aws_ec2_managed_prefix_list.shared_services_management.id]
+  application_cidrs       = [for subnet in data.aws_subnet.application : subnet.cidr_block]
 }
